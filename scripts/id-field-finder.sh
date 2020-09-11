fc8e1ec248b63b0a6f96851c0495308b672b3e47
Hendrik Sokolowski <h.sokolowski@hosting.de>
2020-09-11 16:25:02 +0200

    Add helper script to find id fields

---
+++ scripts/id-field-finder.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+declare swagger_file="swagger.json"
+if [ -n "${1}" ]; then
+    swagger_file="${1}"
+fi
+
+declare mode="get"
+if [ -n "${2}" ]; then
+    mode="${2}"
+fi
+
+echo "Reading file ${swagger_file} in ${mode} mode"
+echo
+
+while read -e path_json; do
+    declare operation_id=$(echo "${path_json}" | jq --raw-output ".${mode}.operationId")
+    while read -e field_json; do
+        declare name=$(echo "${field_json}" | jq --raw-output '.name')
+        declare type=$(echo "${field_json}" | jq --raw-output '.type')
+
+        if [ "${name}" = "id" ] && [ "${type}" != "integer" ]; then
+            echo "## ${operation_id}: field ${name} has type ${type}"
+        fi
+
+        if echo "${name}" | grep '_id' | grep -q -v '_id__'; then
+            if [ "${type}" != "integer" ]; then
+                echo "## ${operation_id}: field ${name} has type ${type}"
+            fi
+        fi
+    done < <(echo "${path_json}" | jq --compact-output ".${mode}.parameters[]")
+done < <(cat "${swagger_file}" | jq --compact-output '.paths[]')
